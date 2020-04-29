b8f7c3a088a6e50fa682d6dbcecefe7c4f36f73e
Russel <russel.jma@gmail.com>
2020-04-28 22:41:14 -0700

    Added imagine comparison test

---
+++ visual_test.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn as nn
+import random
+import torchvision.transforms as transforms
+
+image_size = 64
+
+ngpu = 1
+
+nz = 100
+
+nc = 2
+
+ngf = 64
+
+device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+
+class Generator(nn.Module):
+
+    def __init__(self):
+        super(Generator, self).__init__()
+        self.layers = nn.Sequential(
+            nn.ConvTranspose3d(nz, ngf * 8, 4, 1, 0),
+            nn.BatchNorm3d(ngf * 8),
+            nn.ReLU(True),
+            nn.ConvTranspose3d(ngf * 8, ngf * 4, 4, 2, 1),
+            nn.BatchNorm3d(ngf * 4),
+            nn.ReLU(True),
+            nn.ConvTranspose3d(ngf * 4, ngf * 2, 4, 2, 1),
+            nn.BatchNorm3d(ngf * 2),
+            nn.ReLU(True),
+            nn.ConvTranspose3d(ngf * 2, ngf, 4, 2, 1),
+            nn.BatchNorm3d(ngf),
+            nn.ReLU(True),
+            nn.ConvTranspose3d(ngf, nc, 4, 2, 1),
+        )
+
+    def forward(self, input):
+        out = self.layers(input)
+        return torch.tanh(out)
+
+netG = Generator().to(device)
+netG.load_state_dict(torch.load("gantrial148/gan_models/gen_at_e5000.pt"))
+
+data = torch.load("TRIMMED64.pt")
+data = data.permute(1, 0, 2, 3, 4)[:, 0:2, :, :, :]
+data = data.view(335, 2, 64, 64, 64).to(device)
+
+noise = torch.randn(8, nz, 1, 1, 1, device=device)
+fake = netG(noise)
+
+image_list = []
+alr_selected = []
+for i in range(8):
+    slice = int(torch.rand(1).item() * 335)
+    while slice in alr_selected:
+        slice = int(torch.rand(1).item() * 335)
+    a = data[slice]
+    a[1] = 1
+    a = a[:, 32, :, :]
+    image_list.append(a)
+    b = fake[i]
+    b[1] = 0
+    b = b[:, 32, :, :]
+    image_list.append(b)
+#random.shuffle(image_list)
+
+def save_image(tensor, filename):
+    ndarr = tensor.mul(256).clamp(0, 255).int().byte().cpu()
+    image = transforms.ToPILImage()(ndarr)
+    image.save(filename)
+
+image = None
+for y in range(4):
+    row = None
+    for x in range(4):
+        if row == None:
+            row = image_list[x + y * 4]
+        else:
+            row = torch.cat((row, image_list[x + y * 4]), dim=2)
+    if image == None:
+        image = row
+    else:
+        image = torch.cat((image, row), dim=1)
+save_image(image[0], "scans.png")
+save_image(image[1], "labels.png")
--- wgan.py
+++ wgan.py
@@ -109,18 +109,23 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
         self.layers = nn.Sequential(
             nn.ConvTranspose3d(nz, ngf * 8, 4, 1, 0),
+            nn.Conv3d(ngf * 8, ngf * 8, 3, 1, 1),
             nn.BatchNorm3d(ngf * 8),
             nn.ReLU(True),
             nn.ConvTranspose3d(ngf * 8, ngf * 4, 4, 2, 1),
+            nn.Conv3d(ngf * 4, ngf * 4, 3, 1, 1),
             nn.BatchNorm3d(ngf * 4),
             nn.ReLU(True),
             nn.ConvTranspose3d(ngf * 4, ngf * 2, 4, 2, 1),
+            nn.Conv3d(ngf * 2, ngf * 2, 3, 1, 1),
             nn.BatchNorm3d(ngf * 2),
             nn.ReLU(True),
             nn.ConvTranspose3d(ngf * 2, ngf, 4, 2, 1),
+            nn.Conv3d(ngf, ngf, 3, 1, 1),
             nn.BatchNorm3d(ngf),
             nn.ReLU(True),
             nn.ConvTranspose3d(ngf, nc, 4, 2, 1),
+            nn.Conv3d(nc, nc, 3, 1, 1),
         )
 
     def forward(self, input):
@@ -137,18 +142,23 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
         self.layers = nn.Sequential(
             nn.Conv3d(nc, ndf, 4, 2, 1),
+            nn.Conv3d(ndf, ndf, 3, 1, 1),
             nn.InstanceNorm3d(ndf, affine=True),
             nn.LeakyReLU(0.2, inplace=True),
             nn.Conv3d(ndf, ndf * 2, 4, 2, 1),
+            nn.Conv3d(ndf * 2, ndf * 2, 3, 1, 1),
             nn.InstanceNorm3d(ndf * 2, affine=True),
             nn.LeakyReLU(0.2, inplace=True),
             nn.Conv3d(ndf * 2, ndf * 4, 4, 2, 1),
+            nn.Conv3d(ndf * 4, ndf * 4, 3, 1, 1),
             nn.InstanceNorm3d(ndf * 4, affine=True),
             nn.LeakyReLU(0.2, inplace=True),
             nn.Conv3d(ndf * 4, ndf * 8, 4, 2, 1),
+            nn.Conv3d(ndf * 8, ndf * 8, 3, 1, 1),
             nn.InstanceNorm3d(ndf * 8, affine=True),
             nn.LeakyReLU(0.2, inplace=True),
             nn.Conv3d(ndf * 8, 1, 4, 1, 0),
+            nn.Conv3d(1, 1, 3, 1, 1),
         )
 
     def forward(self, input):
